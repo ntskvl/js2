77195747dd2fc0171b8bd743c7d66065ec50b114
ni <102757866+ntskvl@users.noreply.github.com>
2022-05-05 15:41:11 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,36 @@
+// 1
+
+function nome (b){
+    console.log(b)
+    return
+}
+
+nome ('Nicolly')
+
+// 2
+
+function soma (a,b){
+    let res = x+y;
+    console.log(res)
+    return
+}
+
+soma (5,3)
+
+// 3 
+
+function soma (c,d){
+    console.log(c*d)
+    return
+}
+
+soma (2,4)
+
+// 4
+
+function conta (e,f,g,h){
+    let res = (e+f+g+h)/4
+    console.log(res)
+}
+
+conta (6,7,8,9)
---
+++ funcoes.js
@@ -0,0 +1,8 @@
+//soma
+
+function soma (a,b){
+    console.log(a+b)
+    return
+}
+
+soma (5,3)
